6a1b33a34cf2b151a9680d19e80e5b86ae4ed98d
Muhammed Faizan <156081595+FaizanTahir450@users.noreply.github.com>
2025-12-30 14:25:44 +0500

    Change API base URL to production endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://ai-image-generator-h6o0.onrender.com/api",
 });
 
 export const GetPosts = () => API.get("post");
